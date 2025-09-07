8f62d20c181873456aff8f04942fcb2cb706ec43
XFINL <xiaofins@proton.me>
2025-09-07 21:19:21 +0800

    Update ContributeService.java

--- service/ContributeService.java
+++ service/ContributeService.java
@@ -1,24 +1,16 @@
 package run.halo.contrib.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import run.halo.app.core.extension.content.Post;
+import run.halo.app.core.extension.content.PostRequest;
+import run.halo.app.core.extension.content.PostService;
 import run.halo.contrib.model.ContributeRequest;
 
-@Service
-public class ContributeService {
+import java.time.Instant;
+import java.util.List;
 
-    public void handleContribution(ContributeRequest request) {
-        // 这里暂时只是模拟保存
-        // 以后可以接 Halo 的 Post API 把投稿写入数据库
-        System.out.println("收到投稿：");
-        System.out.println("标题：" + request.getTitle());
-        System.out.println("内容：" + request.getContent());
-        System.out.println("分类：" + request.getCategory());
-        System.out.println("标签：" + request.getTags());
-        if (request.getImage() != null) {
-            System.out.println("图片：" + request.getImage().getOriginalFilename());
-        }
-        if (request.getAttachment() != null) {
-            System.out.println("附件：" + request.getAttachment().getOriginalFilename());
-        }
-    }
-}
+@Service
+@RequiredArgsConstructor
+public class
